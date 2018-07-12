08998115d4572376d63690cfbdff2465e1bb3330
Stan H <deindall@gmail.com>
2018-07-12 16:41:35 +0200

    Ported UnitActionRework

---
+++ Wizardry/UnitActionRework/src/ActionRework.c
@@ -0,0 +1,45 @@
+#include "gbafe.h"
+
+extern int ActionStaffDoorChestUseItem(Proc*) __attribute__((long_call));
+
+typedef int (*ActionFunc) (Proc*);
+
+extern const ActionFunc UnitActionCallTable[];
+
+static int RequiresProcYield(ActionFunc func) {
+	const u32 raw = (const u32)(func);
+	return (raw >> 28) ? 1 : 0;
+}
+
+int ApplyUnitAction(Proc* proc) {
+	gActiveUnit = GetUnit(gActionData.subjectIndex);
+
+#ifndef NO_NIGHTMARE_HARDCODED_CHECK
+
+	if (gActionData.actionIndex == UNIT_ACTION_COMBAT) {
+		if (GetItemIndex(gActiveUnit->items[gActionData.itemSlotIndex]) == 0xA6) {
+			ActionStaffDoorChestUseItem(proc);
+			return 0;
+		}
+	}
+
+#endif // NO_NIGHTMARE_HARDCODED_CHECK
+
+	ActionFunc func = UnitActionCallTable[gActionData.actionIndex];
+
+	if (func) {
+		if (RequiresProcYield(func)) {
+			func(proc);
+			return 0;
+		}
+
+		return func(proc);
+	}
+
+	return 1;
+}
+
+int WaitAction(Proc* proc) {
+	gActiveUnit->state |= US_HAS_MOVED;
+	return 1;
+}
