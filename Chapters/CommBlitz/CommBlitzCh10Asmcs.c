d14b2cd5fc1dcffd1fe95be736083d032a1e8d90
Stan H <deindall@gmail.com>
2019-07-09 00:03:48 +0200

    CommBlitz Ch10 progress

--- Chapters/CommBlitz/CommBlitzCh10Asmcs.c
+++ Chapters/CommBlitz/CommBlitzCh10Asmcs.c
@@ -31,12 +31,54 @@ void ASMC_LoadRescued(struct Proc* proc)
 	gEventSlot[0xC] = FALSE;
 }
 
+/*
+ * ASMC parameters:
+ *   s2 = address of trap data
+ */
+void ASMC_LoadTraps(struct Proc* proc)
+{
+	// TODO: add to CLib
+	extern void LoadTrapData(const void* data);
+
+	const void* data = (const void*)(gEventSlot[2]);
+
+	if (data)
+		LoadTrapData(data);
+}
+
+/*
+ * ASMC result:
+ *   sC = game total turn count up until now
+ */
+void ASMC_GetGameTotalTurnCount(struct Proc* proc)
+{
+	// TODO: add to CLib
+	extern int GetGameTotalTurnCount(void);
+
+	gEventSlot[0xC] = 1 + GetGameTotalTurnCount();
+}
+
+void ASMC_FixActiveUnit(struct Proc* proc)
+{
+	gActiveUnit->state = gActiveUnit->state &~ (US_HIDDEN | US_HAS_MOVED | US_HAS_MOVED_AI);
+}
+
+void ASMC_FixDanGoyle(struct Proc* proc)
+{
+	extern const u8 gDanGoyleCharId;
+
+	struct Unit* unit = GetUnitByCharId(gDanGoyleCharId);
+
+	if (unit)
+		unit->movBonus = 1;
+}
+
 static
 int IsRescuingChar(int charId)
 {
-	extern const u8 gRescuingChars[];
+	extern const u8 gFleetCharList[];
 
-	for (const u8* it = gRescuingChars; *it; ++it)
+	for (const u8* it = gFleetCharList; *it; ++it)
 	{
 		if (*it == charId)
 			return TRUE;
@@ -62,4 +104,15 @@ int ASME_CheckRescues(void* unk)
 	}
 
 	return FALSE;
+}
+
+int AiTransformMoveIntoTalk(const u8* charId)
+{
+	AiUpdateDecision(
+		AI_DECISION_TALK, 0,
+		gActiveUnit->index,
+		GetUnitByCharId(*charId)->index,
+		0xFF);
+
+	return TRUE;
 }
