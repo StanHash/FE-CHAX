0a47af310d6abb788a41573bb12a6082236490b3
Stan H <deindall@gmail.com>
2019-09-17 15:14:53 +0200

    fix LolStats autolevels exploding

--- Wizardry/CLolStats/src/LolStats.c
+++ Wizardry/CLolStats/src/LolStats.c
@@ -1,10 +1,24 @@
 #include "gbafe.h"
 
-int GetStatIncrease(int growth) {
+int NuggetStatIncrease(int growth)
+{
 	int stat = 0;
 	
 	while ((growth -= NextRN_100()) >= 0)
 		stat++;
 	
 	return stat;
+}
+
+int NuggetAutoleveledStatIncrease(int growth, int level)
+{
+	int effGrowth = 0;
+
+	// Here we just go for an approx that assumes growth < 100
+	// It's probably also wrong but probably good enough
+	effGrowth += 10000 * (growth * level + 50); // chances of getting stat 1
+	effGrowth += 100 * (growth * growth * level + 5000) / 2; // chances of getting stat 2
+	effGrowth += (growth * growth * growth * level + 500000) / 4; // chances of getting stat 3
+
+	return Div(effGrowth, 1000000);
 }
