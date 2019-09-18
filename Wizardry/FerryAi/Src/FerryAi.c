e1023a9afdf318ee49597ec1d704abfa0f809d12
Stan H <deindall@gmail.com>
2019-09-18 15:07:29 +0200

    add ASMC_LoadRescued helper to FerryAi

--- Wizardry/FerryAi/Src/FerryAi.c
+++ Wizardry/FerryAi/Src/FerryAi.c
@@ -220,3 +220,33 @@ void ASMC_AddFerryDropTiles(struct Proc* proc)
 		++list;
 	}
 }
+
+/*
+ * ASMC parameters:
+ *   s1 = address of unit definition (one UNIT)
+ *   s2 = char id of rescuer
+ *
+ * ASMC result:
+ *   sC = 1 if the rescued unit was sucessfully loaded, 0 otherwise
+ */
+void ASMC_LoadRescued(struct Proc* proc)
+{
+	const struct UnitDefinition* udef = ((const struct UnitDefinition*)(gEventSlot[1]));
+
+	struct Unit* rescuer = GetUnitByCharId(gEventSlot[2]);
+
+	if (UNIT_IS_VALID(rescuer) && !(rescuer->state & (US_UNAVAILABLE | US_RESCUING | US_RESCUED)))
+	{
+		struct Unit* rescued = LoadUnit(udef);
+
+		if (rescued)
+		{
+			UnitRescue(rescuer, rescued);
+
+			gEventSlot[0xC] = TRUE;
+			return;
+		}
+	}
+
+	gEventSlot[0xC] = FALSE;
+}
