a2e7ea13d46338aefb2dd95568052cac6fda9553
Stan H <deindall@gmail.com>
2019-09-18 14:13:49 +0200

    normalized folder capitalization

--- Wizardry/CLolStats/Src/LolStats.c
+++ Wizardry/CLolStats/Src/LolStats.c
@@ -16,6 +16,7 @@ int NuggetAutoleveledStatIncrease(int growth, int level)
 
 	// Here we just go for an approx that assumes growth < 100
 	// It's probably also wrong but probably good enough
+
 	effGrowth += 10000 * (growth * level + 50); // chances of getting stat 1
 	effGrowth += 100 * (growth * growth * level + 5000) / 2; // chances of getting stat 2
 	effGrowth += (growth * growth * growth * level + 500000) / 4; // chances of getting stat 3
